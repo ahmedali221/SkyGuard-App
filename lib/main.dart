111e892ca03515f9247eed4e77d3d3ec408e4e91
ahmedali <ahmeedali332332@gmail.com>
2025-02-12 14:51:57 +0200

    this is a new update

--- lib/main.dart
+++ lib/main.dart
@@ -7,4 +7,5 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
+  // This is a new comment for pull request
 }
